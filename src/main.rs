c47c9c0ff73880b2ab3fdd8b95e9cd3ed1e9acad
Maciek Mika <maciek@strmprivacy.io>
2022-05-24 16:10:26 +0200

    feat: continues event sending

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use rand::Rng;
 use std::env;
+use std::thread::sleep;
+use std::time::Duration;
 
 use strm_privacy_driver::error::Error;
 use strm_privacy_driver::{StrmPrivacyClient, StrmStatusCode};
@@ -16,6 +19,7 @@ async fn main() -> Result<(), Error> {
     let client_id = args.get(2).unwrap();
     let client_secret = args.get(3).unwrap();
 
+    println!("initializing client");
     let mut strm_privacy_client = StrmPrivacyClient::default(
         billing_id.to_string(),
         client_id.to_string(),
@@ -23,23 +27,29 @@ async fn main() -> Result<(), Error> {
     )
     .await?;
 
-    let event = create_event();
+    println!("sending events..");
+    loop {
+        let event = create_event();
 
-    // catch specific status_codes and decide what to do
-    match strm_privacy_client.send_event(event).await? {
-        (StrmStatusCode::NO_CONTENT, _) => {
-            println!("Success!")
+        // catch specific status_codes and decide what to do
+        match strm_privacy_client.send_event(event).await? {
+            (StrmStatusCode::NO_CONTENT, _) => {
+                println!("Event sent: {}", StrmStatusCode::NO_CONTENT)
+            }
+            (status_code, message) => {
+                println!("{} {}", status_code, message)
+            }
         }
-        (status_code, message) => {
-            println!("{} {}", status_code, message)
-        }
+
+        let duration = Duration::from_millis(500);
+        sleep(duration)
     }
-
-    Ok(())
 }
 
 // create new event based on the example schema
 fn create_event() -> DemoEvent {
+    let consent_level = rand::thread_rng().gen_range(0..4);
+
     DemoEvent {
         strm_meta: StrmMeta {
             event_contract_ref: "strmprivacy/example/1.3.0".to_string(),
@@ -47,7 +57,7 @@ fn create_event() -> DemoEvent {
             timestamp: None,
             key_link: None,
             billing_id: None,
-            consent_levels: vec![0],
+            consent_levels: vec![consent_level],
         },
         unique_identifier: Some("unique".to_string()),
         consistent_value: "consistent".to_string(),
